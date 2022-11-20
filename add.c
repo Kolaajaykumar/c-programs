40a3007e91f44925f85fb559e84d51208c82bdfe
Kolaajaykumar <115888740+Kolaajaykumar@users.noreply.github.com>
2022-11-20 12:39:17 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    printf("enter the velue of b:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("%d+%d=%d",a,b,c);
+    getch();
+
+    return 0;
+
+}
+
---
+++ evennumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("enter the number a");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("%d is a even number",a);
+    
+    getch();
+    }
+    return 0;
+}
